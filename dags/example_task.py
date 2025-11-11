5121d5150e4f0da80784c3b8ec5be3b0beee0865
giangnt <giangnguyen3246@gmail.com>
2025-11-11 09:26:02 +0700

    chore: example job for test spark

--- dags/example_task.py
+++ dags/example_task.py
@@ -9,13 +9,13 @@ default_args = {
 with DAG(
     dag_id="spark_submit_example",
     start_date=datetime(2025, 1, 1),
-    schedule_interval=None,
+    schedule=None,
     default_args=default_args,
     catchup=False
 ):
     spark_job = SparkSubmitOperator(
         task_id="spark_pi",
-        application="/opt/spark-apps/pi.py",     # path to your spark app
+        application="/opt/spark-apps/main.py",     # path to your spark app
         conn_id="spark_default",                # Airflow Spark Connection
         verbose=True,
         executor_memory="1g",
